7246c10977bf0a48298bb3db4d4392a384379d3e
Zaid <zaidzs.2024@gmail.com>
2025-07-25 10:38:03 +0530

    Update settings.py

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '%6lp_p!%r$7t-2ql5hc5(r@)8u_fc+6@ugxcnz=h=b(fn#3$p9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['digicast.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
